f863b7a13d89c659a9829c2c6419d6bc25d08730
hafsaaltafdev <hafsaltaf.dev09@gmail.com>
2026-06-24 23:28:25 +0500

    remove .env from tracking

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+
+  if (isConnected) return;
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.log(" MongoDB connection failed:", error.message);
--- server.js
+++ server.js
@@ -12,14 +12,20 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(async (req, res, next) => {
+  await connectDB(); // ← ye add karo
+  next();
+});
+
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, ()=>{
+//     console.log(`Server running on port ${PORT}`);
     
-})
+// })
+
+export default app;
