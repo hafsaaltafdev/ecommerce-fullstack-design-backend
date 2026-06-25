ca91c5481cb03ecdf4869f5204a41d88f4b831a5
hafsaaltafdev <hafsaltaf.dev09@gmail.com>
2026-06-25 16:15:01 +0500

    add profile functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -98,3 +98,24 @@ export const loginUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// get user for profile
+export const getProfile = async (req, res) => {
+    try {
+
+        const user = await User.findById(req.user.id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signupUser, loginUser } from "../controllers/authController.js";
+import { signupUser, loginUser, getProfile } from "../controllers/authController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/profile", authMiddleware, getProfile);
 
 export default router;
